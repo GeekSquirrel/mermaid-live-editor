2b6bd55bf5099fc04aec1169aa66d9b6320f8706
Geek Squirrel <geeksquirrel@qq.com>
2026-09-03 09:58:06 +0800

    build(frontend): use vitePreprocess for Svelte 5 compatibility in workspace

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import 'dotenv/config';
-import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: [sveltePreprocess({})],
+  preprocess: [vitePreprocess()],
+
   kit: {
     alias: {
       '$/*': './src/lib/*'
